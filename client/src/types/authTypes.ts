0e9fdb2d06f819155fd841818c9cfa59f6ac2a57
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2025-12-23 14:01:53 -0600

    *Cambios por migracion a uso de zod

--- client/src/types/authTypes.ts
+++ client/src/types/authTypes.ts
@@ -1,12 +1,5 @@
 import type { RegLogResSchema } from "../schemas/authSchemas";
 
-//Payload de login o registro de usuario
-// export interface UserLogReg {
-//      name?: string,
-//      email: string,
-//      password: string
-// }
-//Rol de usuario, 0 base 1 admin
 type RolType = 0 | 1;
 
 //Respuesta de login o registro
