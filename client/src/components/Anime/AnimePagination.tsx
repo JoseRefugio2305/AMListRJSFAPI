3e179f0540ee18c0f16c02f22b0b45fab4c7dd0c
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2026-01-05 17:02:11 -0600

    *Cambio de apariencia de paginacion de anime para agregar una nueva vista de lista

--- client/src/components/Anime/AnimePagination.tsx
+++ client/src/components/Anime/AnimePagination.tsx
@@ -1,7 +1,9 @@
-import { Paginator, type PaginatorPageChangeEvent } from "primereact/paginator";
-import { AnimeCard } from "./AnimeCard";
-import { NotResultsFound } from "../Common/NotResultsFound";
+import { useState } from "react";
+import { DataView, DataViewLayoutOptions } from "primereact/dataview";
 import type { AnimeSchema } from "../../schemas/animeSchemas";
+import { AnimeCard } from "../Anime/AnimeCard";
+import { AnimeListCard } from "../Anime/AnimeListCard";
+import { Paginator, type PaginatorPageChangeEvent } from "primereact/paginator";
 
 interface AnimePaginationProps {
    page: number;
@@ -10,7 +12,6 @@ interface AnimePaginationProps {
    setPage: (first: number) => void;
    animes: AnimeSchema[];
 }
-
 export function AnimePagination({
    page,
    total,
@@ -18,36 +19,73 @@ export function AnimePagination({
    setPage,
    animes,
 }: AnimePaginationProps) {
+   const [layout, setLayout] = useState<"list" | "grid">("grid");
+
    const handlePageChange = (event: PaginatorPageChangeEvent) => {
       setLoading(true);
       setPage(event.first / 20 + 1);
    };
 
+   const listItem = (anime: AnimeSchema) => {
+      return <AnimeListCard {...anime} key={anime.id} />;
+   };
+
+   const gridItem = (anime: AnimeSchema) => {
+      return <AnimeCard {...anime} key={anime.id} />;
+   };
+
+   const itemTemplate = (product: AnimeSchema, layout: string) => {
+      if (!product) {
+         return;
+      }
+
+      if (layout === "list") return listItem(product);
+      else if (layout === "grid") return gridItem(product);
+   };
+
+   const listTemplate = (products: AnimeSchema[], layout: "list" | "grid") => {
+      return layout === "grid" ? (
+         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+            {products.map((product) => itemTemplate(product, layout))}
+         </div>
+      ) : (
+         <div className="grid grid-cols-1">
+            {products.map((product) => itemTemplate(product, layout))}
+         </div>
+      );
+   };
+
+   const header = () => {
+      return (
+         <div className="flex justify-end">
+            <DataViewLayoutOptions
+               layout={layout}
+               onChange={(e) => setLayout(e.value === "list" ? "list" : "grid")}
+            />
+         </div>
+      );
+   };
+
    return (
-      <>
-         {animes.length > 0 ? (
-            <>
-               <Paginator
-                  first={(page - 1) * 20}
-                  rows={20}
-                  totalRecords={total}
-                  onPageChange={handlePageChange}
-               />
-               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
-                  {animes.map((anime: AnimeSchema) => (
-                     <AnimeCard {...anime} key={anime.id} />
-                  ))}
-               </div>
-               <Paginator
-                  first={(page - 1) * 20}
-                  rows={20}
-                  totalRecords={total}
-                  onPageChange={handlePageChange}
-               />
-            </>
-         ) : (
-            <NotResultsFound message="No hay Animes que coincidan con la búsqueda." />
-         )}
-      </>
+      <div className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow">
+         <Paginator
+            first={(page - 1) * 20}
+            rows={20}
+            totalRecords={total}
+            onPageChange={handlePageChange}
+         />
+         <DataView
+            value={animes}
+            listTemplate={listTemplate}
+            layout={layout}
+            header={header()}
+         />
+         <Paginator
+            first={(page - 1) * 20}
+            rows={20}
+            totalRecords={total}
+            onPageChange={handlePageChange}
+         />
+      </div>
    );
 }
