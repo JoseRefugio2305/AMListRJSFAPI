ad679e70a3922f2d66456f78bbd40dea182d4034
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2026-01-23 15:55:56 -0600

    *Correccion estilos de sidebar dashboard

--- client/src/components/Dashboard/SidebarDash.tsx
+++ client/src/components/Dashboard/SidebarDash.tsx
@@ -18,7 +18,7 @@ export function SidebarDash({ selectedOption }: SidebarDashProps) {
    const [visible, setVisible] = useState<boolean>(false);
 
    const getClassLink = (opt: number) => {
-      return `hover:underline hover:bg-purple-600 dark:hover:bg-purple-600 hover:text-white font-semibold text-md bg-purple-500 dark:bg-purple-500 rounded-3xl px-2 py-1 w-full justify-center flex items-center text-white my-3 ${
+      return `hover:underline hover:bg-purple-600 dark:hover:bg-purple-600 hover:text-white font-semibold text-md bg-purple-500 dark:bg-purple-500 rounded-3xl px-2 py-1 w-full justify-center flex flex-row items-center text-white my-3 ${
          selectedOption === opt && "underline bg-purple-600 dark:bg-purple-600"
       }`;
    };
@@ -27,34 +27,47 @@ export function SidebarDash({ selectedOption }: SidebarDashProps) {
       <>
          <Sidebar visible={visible} onHide={() => setVisible(false)}>
             <h2 className="text-2xl font-bold flex flex-row gap-4">
-               <ChartArea size={28} /> Dashboard
+               <ChartArea size={28} className="w-[20%]" />{" "}
+               <p className="w-[80%]">Dashboard</p>
             </h2>
             <Link to="/dashboard" className={getClassLink(1)}>
-               <ChartArea size={28} /> Dasboard
+               <ChartArea size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Dashboard</p>
             </Link>
             <Link to="/dashboard/genreslist" className={getClassLink(4)}>
-               <ListCheck size={28} /> Géneros
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Géneros</p>
             </Link>
             <h2 className="text-2xl font-bold flex flex-row gap-4">
-               <TvMinimalPlayIcon size={28} /> Animes
+               <TvMinimalPlayIcon
+                  size={28}
+                  className="w-[20%] justify-center"
+               />{" "}
+               <p className="w-[80%] text-center">Animes</p>
             </h2>
             <Link to="/dashboard/animelist" className={getClassLink(2)}>
-               <ListCheck size={28} /> Lista de Animes
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Lista de Animes</p>
             </Link>
             <Link to="/dashboard/studioslist" className={getClassLink(5)}>
-               <ListCheck size={28} /> Estudios de Animación
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Estudios de Animación</p>
             </Link>
             <h2 className="text-2xl font-bold flex flex-row gap-4">
-               <Brush size={28} /> Mangas
+               <Brush size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Mangas</p>
             </h2>
             <Link to="/dashboard/mangalist" className={getClassLink(3)}>
-               <ListCheck size={28} /> Lista de Mangas
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Lista de Mangas</p>
             </Link>
             <Link to="/dashboard/editorialslist" className={getClassLink(6)}>
-               <ListCheck size={28} /> Editoriales de Manga
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Editoriales de Manga</p>
             </Link>
             <Link to="/dashboard/authorslist" className={getClassLink(7)}>
-               <ListCheck size={28} /> Autores de Manga
+               <ListCheck size={28} className="w-[20%] justify-center" />{" "}
+               <p className="w-[80%] text-center">Autores de Manga</p>
             </Link>
          </Sidebar>
          <Button onClick={() => setVisible(!visible)} color="purple">
