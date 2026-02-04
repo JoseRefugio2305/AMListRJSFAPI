42c6442c20366d491f5ff5019530de97991ec4be
JoseRefugio2305 <joserefugioriveramendoza@gmail.com>
2026-02-04 15:56:30 -0600

    -Eliminacion de helpers en core/database sus funciones fueron movidad a repositories

--- api/app/core/database/__init__.py
+++ api/app/core/database/__init__.py
@@ -1,36 +1,3 @@
 from .connection import connect_mongo, close_mongo
-from .helpers.db_helpers import (
-    lookup_user_favorites,
-    filtrado_tipos,
-    filtro_emision,
-    filtrado_info_incompleta,
-    apply_paginacion_ordenacion,
-    filtrado_gsae,
-)
-from .helpers.manga_helpers import filtrado_busqueda_avanzada_manga, get_full_manga
-from .helpers.anime_helpers import filtrado_busqueda_avanzada_anime, get_full_anime
-from .helpers.stats_db_helpers import (
-    statsTipo,
-    statsGenero,
-    topEditoriales,
-    topEstudios,
-)
 
-__all__ = [
-    "connect_mongo",
-    "close_mongo",
-    "lookup_user_favorites",
-    "filtrado_busqueda_avanzada_anime",
-    "filtrado_busqueda_avanzada_manga",
-    "filtrado_tipos",
-    "filtro_emision",
-    "statsTipo",
-    "statsGenero",
-    "topEditoriales",
-    "topEstudios",
-    "filtrado_info_incompleta",
-    "apply_paginacion_ordenacion",
-    "get_full_manga",
-    "get_full_anime",
-    "filtrado_gsae",
-]
+__all__ = ["connect_mongo", "close_mongo"]
